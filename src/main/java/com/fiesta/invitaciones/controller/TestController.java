bb695ef1fe734b3335200520c3fb5272a125daa5
Abdiel <mendozaac03@gmail.com>
2026-06-15 17:16:03 -0600

    abg3

--- src/main/java/com/fiesta/invitaciones/controller/TestController.java
+++ src/main/java/com/fiesta/invitaciones/controller/TestController.java
@@ -2,17 +2,20 @@ package com.fiesta.invitaciones.controller;
 
 
 import com.fiesta.invitaciones.mongo.Photo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
 
     @GetMapping("/hola")
     public String like() {
+        log.info("entre a revisar**********************************");
         return "hola perro";
     }
 }
