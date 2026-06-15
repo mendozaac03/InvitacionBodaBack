33592be0c9732e84e2cd86f1ad28d85da1955450
Abdiel <mendozaac03@gmail.com>
2026-06-15 17:37:33 -0600

    abg4

--- src/main/java/com/fiesta/invitaciones/InvitacionesApplication.java
+++ src/main/java/com/fiesta/invitaciones/InvitacionesApplication.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+//import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 
