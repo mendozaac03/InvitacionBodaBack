bc67fb47a9b4a0ffe5b88847c649993441f0f48e
Abdiel <mendozaac03@gmail.com>
2026-06-15 14:24:58 -0600

    add

--- src/main/java/com/fiesta/invitaciones/InvitacionesApplication.java
+++ src/main/java/com/fiesta/invitaciones/InvitacionesApplication.java
@@ -2,8 +2,10 @@ package com.fiesta.invitaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class InvitacionesApplication {
 
 	public static void main(String[] args) {
