45c595f0feae079ded305567a8a4ca96f8c1e1ab
Abdiel <mendozaac03@gmail.com>
2026-06-15 18:03:50 -0600

    abg10

--- src/main/java/com/fiesta/invitaciones/controller/TestController.java
+++ src/main/java/com/fiesta/invitaciones/controller/TestController.java
@@ -2,11 +2,16 @@ package com.fiesta.invitaciones.controller;
 
 
 import com.fiesta.invitaciones.mongo.Photo;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -18,4 +23,38 @@ public class TestController {
         log.info("entre a revisar**********************************");
         return "hola perro";
     }
-}
+
+
+        @GetMapping("/mongo")
+        public Map<String,Object> testMongo() {
+
+            Map<String,Object> response =
+                    new HashMap<>();
+
+            try {
+
+                MongoClient mongoClient =
+                        MongoClients.create(
+                                System.getenv("MONGODB_URI"));
+
+                mongoClient.listDatabaseNames()
+                        .forEach(System.out::println);
+
+                response.put("status", "OK");
+
+            } catch (Exception e) {
+
+                response.put(
+                        "error",
+                        e.getClass().getName());
+
+                response.put(
+                        "message",
+                        e.getMessage());
+
+            }
+
+            return response;
+        }
+    }
+
